57611d1122cd5badc07f51df50d6d82501ea9324
Raphael Schaller <raphael.schaller@roboception.de>
2017-09-26 13:57:34 +0200

    initial commit

---
+++ src/main.cc
@@ -0,0 +1,46 @@
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <std_msgs/Int8.h>
+
+const std::string topic = "test_topic";
+
+void subscriberCallback(const std_msgs::Int8::ConstPtr &msg)
+{
+  std::cout << "Callback" << std::endl;
+} 
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "callbackqueue_clear_example");
+
+  ros::CallbackQueue cq;
+
+  ros::NodeHandle nh;
+  nh.setCallbackQueue(&cq);
+
+  auto subscriber = nh.subscribe(topic, 1, subscriberCallback);
+  auto publisher = nh.advertise<std_msgs::Int8>(topic, 1);
+
+  // subscriber is working => "Callback" will be printed
+  std::cout << "will call callback" << std::endl;
+  publisher.publish(std_msgs::Int8::Ptr(new std_msgs::Int8()));
+  cq.callAvailable();
+
+  // publish message => subscriber queue is full
+  publisher.publish(std_msgs::Int8::Ptr(new std_msgs::Int8()));
+  // clear callback queue
+  cq.clear();
+  
+  // subscriber is broken => no "Callback" will be printed any more
+  std::cout << "will not call callback" << std::endl;
+  publisher.publish(std_msgs::Int8::Ptr(new std_msgs::Int8()));
+  cq.callAvailable();
+
+  // resubscribe
+  subscriber = nh.subscribe(topic, 1, subscriberCallback);
+  
+  // working again => "Callback" will be printed
+  std::cout << "will call callback" << std::endl; 
+  publisher.publish(std_msgs::Int8::Ptr(new std_msgs::Int8()));
+  cq.callAvailable();
+}
